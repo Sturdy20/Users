efcc0a6265ed0dd95e71a4bcd28d0b61302e9632
Sturdy10 <stg100t@gmail.com>
2024-03-22 17:38:54 +0700

    3_22_2024

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -32,12 +32,20 @@ func (h *handler) AddRegisteHandler(c *gin.Context) {
 	}
 	err := h.s.AddRegisterService(register)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "Error", "message": err.Error()})
+		switch err.Error() {
+		case "Failed to query members table":
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "Error", "message": "Failed to register. Please try again later."})
+		case "Email already exists":
+			c.JSON(http.StatusBadRequest, gin.H{"status": "Error", "message": "Email already exists"})
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "Error", "message": "An error occurred. Please try again later."})
+		}
 		return
 	}
 
-	c.JSON(http.StatusCreated, gin.H{"status": "OK", "message": "Register Successfully"})
+	c.JSON(http.StatusCreated, gin.H{"status": "OK", "message": "Register successfully"})
 }
+
 
 
 
@@ -51,10 +59,17 @@ func (h *handler) AddLoginHandler(c *gin.Context) {
 
 	err := h.s.AddLoginService(login)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"status": "Error", "message": err.Error()})
+		switch err.Error() {
+		case "user not found":
+			c.JSON(http.StatusNotFound, gin.H{"status": "Error", "message": "User not found. Please check your email and try again."})
+		case "incorrect password":
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "Error", "message": "Incorrect password. Please try again."})
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"status": "Error", "message": "An error occurred. Please try again later."})
+		}
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "OK", "message": "login Successfully"})
+	c.JSON(http.StatusOK, gin.H{"status": "OK", "message": "Login successful!"})
 }
 
